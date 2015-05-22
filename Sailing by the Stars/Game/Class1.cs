7c0d94cabfaef06af8d1400e6a955ea716ce7a8b
Qian Xiong <qx2dk@virginia.edu>
2015-05-22 16:13:59 -0400

    test: planet class init commit

---
+++ Sailing by the Stars/Game/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sailing_by_the_Stars.Game
+{
+    class Planet
+    {
+        public int Radius;
+        public int Mass;
+        public Vector2 Position;
+        public Vector2 Velocity;
+
+        public Planet(int x = 0, int y = 0, int m = 100, int r = 100)
+        {
+            Radius = r;
+            Mass = m;
+            Position = new Vector2(x, y);
+            Velocity = new Vector2(x, y);
+        }
+
+        public void Move(Vector2 force)
+        {
+            Vector2 Acceleration = new Vector2(force.X / Mass, force.Y / Mass); // a = F/m
+            Velocity.X += Acceleration.X;
+            Velocity.Y += Acceleration.Y;
+            Position.X += Velocity.X;
+            Position.Y += Velocity.Y;
+        }
+    }
+
+}
