e5aaa58e19f6071bee09ef088cbcd52d7a3d5b20
AlishanHassan <ah2xt@virginia.edu>
2015-06-13 06:21:53 -0400

    Added names in comments

--- Sailing by the Stars/MainGame.cs
+++ Sailing by the Stars/MainGame.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿//Alishan Hassan, Qian Xiong, Andrew Lee
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Audio;
